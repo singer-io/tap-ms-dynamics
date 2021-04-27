1ea17ea2470c19a00364b558e1ede6c2c3cf803d
Andrey Kabanov <andrey.kabanov@bytecode.io>
2021-04-27 14:02:59 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+requirements_file = "requirements.txt"
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="tap-dynamics",
+    version="0.1.0",
+    description="Singer.io tap for the Microsoft Dataverse Web API",
+    long_description=readme,
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_dynamics"],
+    install_requires=open(requirements_file).readlines(),
+    entry_points="""
+    [console_scripts]
+    tap-dynamics=tap_dynamics:main
+    """,
+    packages=find_packages(exclude=["tests"]),
+    package_data = {
+        "schemas": ["tap_dynamics/schemas/*.json"]
+    },
+    include_package_data=True,
+)
