818666e490f2d6e81e2c2f879e69699efedb2931
Andrey Kabanov <andrey.kabanov@bytecode.io>
2021-05-11 19:21:52 -0700

    adding transform.py code

---
+++ tap_dynamics/transform.py
@@ -0,0 +1,12 @@
+import json
+
+def flatten_entity_attributes(attributes):
+    flat_attributes = {}
+    # dict of attributes/fields for an entity in {'LogicalNameOfAttribute': 'DynamicsDataType', ...}
+    for attr in attributes:
+        logical_name = attr.get('LogicalName')
+        dynamics_type = attr.get('AttributeTypeName').get('Value')
+        is_readable = attr.get('IsValidForRead')
+        flat_attributes.update({logical_name: {'type': dynamics_type, 'is_readable': is_readable}})
+
+    return flat_attributes
