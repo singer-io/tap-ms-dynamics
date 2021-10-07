1d2a7127e9160abedf3621aaf19324a5a0d4823c
Andy Lu <andy@stitchdata.com>
2021-10-07 09:48:59 -0400

    Remove long_description, merge in requirements.txt (#3)
    * Remove long_description, merge in requirements.txt
    
    * Add barebones circleci config

--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
 from setuptools import find_packages, setup
 
-requirements_file = "requirements.txt"
-
-with open("README.md") as f:
-    readme = f.read()
-
 setup(
     name="tap-ms-dynamics",
     version="0.1.0",
     description="Singer.io tap for the Microsoft Dataverse Web API",
-    long_description=readme,
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_dynamics"],
-    install_requires=open(requirements_file).readlines(),
+    install_requires=[
+        "backoff==1.8.0",
+        "certifi==2020.12.5",
+        "chardet==4.0.0",
+        "ciso8601==2.1.3",
+        "idna==2.10",
+        "jsonschema==2.6.0",
+        "python-dateutil==2.8.1",
+        "pytz==2018.4",
+        "requests==2.25.1",
+        "simplejson==3.11.1",
+        "singer-python==5.10.0",
+        "six==1.15.0",
+        "urllib3==1.26.3",
+    ],
     entry_points="""
     [console_scripts]
     tap-ms-dynamics=tap_dynamics:main
